ef7b1d3a33b134c554e0d1a754ed8355f0606db7
Jagdish Patil <jagdish.patil@silicus.com>
2020-01-19 12:26:15 +0530

    Update app.js

--- landingpage/app.js
+++ landingpage/app.js
@@ -9,5 +9,5 @@ const t1 = new TimelineMax();
 t1.fromTo(hero, 1.2, {height: '0%'}, {height: '80%', ease: Power2.easeInOut}).
 fromTo(hero, 1.2, {width: '100%'}, {width: '80%', ease: Power2.easeInOut}).
 fromTo(slider, 1.2, {x: '-100%'}, {x: '0%', ease: Power2.easeInOut},"-=1.2").
-fromTo(logo, 0.5 , {opacity: 0, x: 30}, {opacity: 1,x: 0},"-=0.5").
+fromTo(logo, 0.5 , {opacity: 0, x: 30}, {opacity: 1,x: 0},"-=0.5"). 
 fromTo(harmburger, 0.5 , {opacity: 0, x: 30}, {opacity: 1,x: 0},"-=0.5");
