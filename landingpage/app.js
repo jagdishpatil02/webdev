467b4c8412b657c45d3cf60fc11dd25700051bc7
Jagdish Patil <patil.jagdish222@gmail.com>
2023-05-18 10:47:06 +0530

    changed the folder structure and solved the error in landing page

--- landingpage/app.js
+++ landingpage/app.js
@@ -1,16 +1,33 @@
-const hero = document.querySelector('.hero');
-const slider = document.querySelector('.slider');
-const logo = document.querySelector('.logo');
-const harmburger = document.querySelector('.menu-logo');
-
-
+const hero = document.querySelector(".hero");
+const slider = document.querySelector(".slider");
+const logo = document.querySelector(".logo");
+const harmburger = document.querySelector(".menu-logo");
 
 const t1 = new TimelineMax();
-t1.fromTo(hero, 1.2, {height: '0%'}, {height: '80%', ease: Power2.easeInOut}).
-fromTo(hero, 1.2, {width: '100%'}, {width: '80%', ease: Power2.easeInOut}).
-fromTo(slider, 1.2, {x: '-100%'}, {x: '0%', ease: Power2.easeInOut},"-=1.2").
-fromTo(logo, 0.5 , {opacity: 0, x: 30}, {opacity: 1,x: 0},"-=0.5"). 
-fromTo(harmburger, 0.5 , {opacity: 0, x: 30}, {opacity: 1,x: 0},"-=0.5");
-
-{
-
+t1.fromTo(
+  hero,
+  1.2,
+  { height: "0%" },
+  { height: "80%", ease: Power2.easeInOut }
+)
+  .fromTo(
+    hero,
+    1.2,
+    { width: "100%" },
+    { width: "80%", ease: Power2.easeInOut }
+  )
+  .fromTo(
+    slider,
+    1.2,
+    { x: "-100%" },
+    { x: "0%", ease: Power2.easeInOut },
+    "-=1.2"
+  )
+  .fromTo(logo, 0.5, { opacity: 0, x: 30 }, { opacity: 1, x: 0 }, "-=0.5")
+  .fromTo(
+    harmburger,
+    0.5,
+    { opacity: 0, x: 30 },
+    { opacity: 1, x: 0 },
+    "-=0.5"
+  );
