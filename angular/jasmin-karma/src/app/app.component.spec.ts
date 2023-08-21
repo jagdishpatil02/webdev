3f6724de165fe75a359a23c3d3acec58a3bd326e
Jagdish Patil <patil.jagdish222@gmail.com>
2023-08-21 11:41:16 +0530

    test cases updated

--- angular/jasmin-karma/src/app/app.component.spec.ts
+++ angular/jasmin-karma/src/app/app.component.spec.ts
@@ -3,9 +3,12 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { AppService } from './app.service';
+import { of } from 'rxjs';
 describe('AppComponent', () => {
   let component: AppComponent;
   let fixture: ComponentFixture<AppComponent>;
+  let service: AppService;
 
   beforeEach(() =>
     TestBed.configureTestingModule({
@@ -15,6 +18,7 @@ describe('AppComponent', () => {
   );
   beforeEach(() => {
     fixture = TestBed.createComponent(AppComponent);
+    service = TestBed.inject(AppService);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
@@ -28,5 +32,70 @@ describe('AppComponent', () => {
   it('should subtract numbers correctly', () => {
     let result = component.subtract(2, 1);
     expect(result).toEqual(1);
+  });
+
+  it('should fetch data from the API', () => {
+    const testData = [
+      {
+        gender: 'male',
+        name: {
+          title: 'Mr',
+          first: 'Kaspian',
+          last: 'Sund',
+        },
+        location: {
+          street: {
+            number: 101,
+            name: 'Rundhaugveien',
+          },
+          city: 'Leirvik',
+          state: 'Vest-Agder',
+          country: 'Norway',
+          postcode: '6220',
+          coordinates: {
+            latitude: '55.6425',
+            longitude: '-21.3266',
+          },
+          timezone: {
+            offset: '+4:30',
+            description: 'Kabul',
+          },
+        },
+        email: 'kaspian.sund@example.com',
+        login: {
+          uuid: '7da0b7e4-f225-407e-8e06-310621bf6a08',
+          username: 'angrygorilla307',
+          password: 'tracey',
+          salt: '8iiwaD5W',
+          md5: '117987d9722b8fb5bee9aae9df4b04d7',
+          sha1: '903ed68e1e543fcedacdfada04d24e425478c7e5',
+          sha256:
+            'b2af09e7ccdc0b1101040e12a8b2c6bd18752ce6918b8469deb8d9d9898ab507',
+        },
+        dob: {
+          date: '1948-11-09T21:27:30.452Z',
+          age: 72,
+        },
+        registered: {
+          date: '2002-09-23T02:02:39.498Z',
+          age: 18,
+        },
+        phone: '30794605',
+        cell: '46208786',
+        id: {
+          name: 'FN',
+          value: '09114814943',
+        },
+        picture: {
+          large: 'https://randomuser.me/api/portraits/men/40.jpg',
+          medium: 'https://randomuser.me/api/portraits/med/men/40.jpg',
+          thumbnail: 'https://randomuser.me/api/portraits/thumb/men/40.jpg',
+        },
+        nat: 'NO',
+      },
+    ];
+    spyOn(service, 'fetchData').and.returnValue(of(testData));
+    component.ngOnInit();
+    expect(component.employeeData.length).toEqual(1);
   });
 });
--- angular/jasmin-karma/src/app/app.service.spec.ts
+++ angular/jasmin-karma/src/app/app.service.spec.ts
@@ -1,14 +1,17 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AppService } from './app.service';
 import {
   HttpClientTestingModule,
   HttpTestingController,
 } from '@angular/common/http/testing';
+import { AppComponent } from './app.component';
 
 describe('AppService', () => {
   let service: AppService;
   let httpMock: HttpTestingController;
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
   beforeEach(() =>
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
@@ -18,6 +21,8 @@ describe('AppService', () => {
   beforeEach(() => {
     service = TestBed.inject(AppService);
     httpMock = TestBed.inject(HttpTestingController);
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
   });
 
   afterEach(() => {
@@ -26,33 +31,5 @@ describe('AppService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
-  });
-  it('should fetch data from the API', () => {
-    const testData = { name: 'John Doe' };
-
-    service.fetchData().subscribe((data) => {
-      expect(data).toEqual(testData);
-    });
-
-    const req = httpMock.expectOne(
-      'https://gist.githubusercontent.com/HariKrishnaE-1288/ed851de051b8e78af8d0011a12747155/raw/12b3217ae6d33b007be9be02f654716a7226d2f9/employees.04052021.json'
-    );
-    expect(req.request.method).toBe('GET');
-    req.flush(testData);
-  });
-
-  it('should contain specific data in the API response', () => {
-    const expectedData = { id: 1, name: 'John Doe', age: 25 };
-
-    service.fetchData().subscribe((response) => {
-      console.log('expectedData', expectedData);
-      console.log('rs', response);
-      expect(response).toContain(expectedData);
-    });
-
-    const req = httpMock.expectOne(
-      'https://gist.githubusercontent.com/HariKrishnaE-1288/ed851de051b8e78af8d0011a12747155/raw/12b3217ae6d33b007be9be02f654716a7226d2f9/employees.04052021.json'
-    );
-    req.flush([expectedData]);
   });
 });
