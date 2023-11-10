f7197bebe5a5fa1615281ef24ab6e8daf7632fac
Jagdish Patil <patil.jagdish222@gmail.com>
2023-11-10 20:11:45 +0530

    resent otp functionality added

--- animations/otp/index.js
+++ animations/otp/index.js
@@ -1,28 +1,41 @@
 var otpServer = [7, 7, 7, 7, 7];
 
-const otpLocal = [];
-const otpInputs = document.getElementsByClassName('otp-input');
-const message = document.getElementById('message');
+let otpLocal = [];
+const otpInputs = document.getElementsByClassName("otp-input");
+const message = document.getElementById("message");
+const resentOTP = document.getElementById("resentOTP");
+
 const otpInputsArr = Array.from(otpInputs);
 otpInputsArr.forEach(function (control) {
-  control.addEventListener('keyup', function (event) {
+  control.addEventListener("keyup", function (event) {
     // Focus on the next sibling
     otpLocal.push(control.value);
     if (control.nextElementSibling) {
       control.nextElementSibling.focus();
     } else {
       otpInputsArr.forEach(function (control) {
-        if (otpLocal.sort().join(',') === otpServer.sort().join(',')) {
-          control.classList.add('primary-success');
-          message.innerHTML = 'OTP is correct, Wait for a moment !';
-          message.classList.add('text-success');
+        if (otpLocal.sort().join(",") === otpServer.sort().join(",")) {
+          control.classList.add("primary-success");
+          message.innerHTML = "OTP is correct, Wait for a moment !";
+          message.classList.add("text-success");
+          resentOTP.innerHTML = "";
         } else {
-          control.classList.add('primary-danger');
-
-          message.innerHTML = 'Wrong OTP...Only 2 attempts left !';
-          message.classList.add('text-danger');
+          control.classList.add("primary-danger");
+          message.innerText = "Wrong OTP...Only 2 attempts left !";
+          resentOTP.innerText = "Resend OTP";
+          message.classList.add("text-danger");
         }
       });
     }
+  });
+});
+
+document.getElementById("resentOTP").addEventListener("click", function () {
+  otpInputsArr.forEach(function (control) {
+    control.value = null;
+    control.classList.remove("primary-danger");
+    message.innerHTML = "";
+    message.classList.remove("text-danger");
+    otpLocal = [];
   });
 });
