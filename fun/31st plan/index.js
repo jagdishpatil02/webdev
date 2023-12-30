bebcad3a56b2509179f66e045d42adb6004c99ff
Jagdish Patil <patil.jagdish222@gmail.com>
2023-12-30 15:53:23 +0530

    clouseres ex and 31st fun project added

--- fun/31st plan/index.js
+++ fun/31st plan/index.js
@@ -1,13 +1,35 @@
-function moveButton() {
-  var movingBtn = document.getElementById("movingButton");
+function addMouseMoveListener(elementId) {
+  document.body.addEventListener("mousemove", function (event) {
+    let movingBtn = document.getElementById(elementId);
 
-  var buttonRect = movingBtn.getBoundingClientRect();
-  var windowWidth = window.innerWidth - buttonRect.width;
-  var windowHeight = window.innerHeight - buttonRect.height;
+    let buttonRect = movingBtn.getBoundingClientRect();
+    let windowWidth = window.innerWidth - buttonRect.width;
+    let windowHeight = window.innerHeight - buttonRect.height;
 
-  var randomLeft = Math.floor(Math.random() * windowWidth);
-  var randomTop = Math.floor(Math.random() * windowHeight);
+    let distanceThreshold = 50;
 
-  movingBtn.style.left = randomLeft + "px";
-  movingBtn.style.top = randomTop + "px";
+    let mouseX = event.clientX;
+    let mouseY = event.clientY;
+
+    let buttonX = buttonRect.left + buttonRect.width / 2;
+    let buttonY = buttonRect.top + buttonRect.height / 2;
+
+    let distanceToButton = distance(mouseX, mouseY, buttonX, buttonY);
+
+    if (distanceToButton < distanceThreshold) {
+      let randomLeft = Math.floor(Math.random() * windowWidth);
+      let randomTop = Math.floor(Math.random() * windowHeight);
+
+      movingBtn.style.left = randomLeft + "px";
+      movingBtn.style.top = randomTop + "px";
+    }
+  });
 }
+
+function distance(x1, y1, x2, y2) {
+  return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+}
+
+// Add listeners for both buttons
+addMouseMoveListener("movingButton");
+addMouseMoveListener("movingButtonError");
