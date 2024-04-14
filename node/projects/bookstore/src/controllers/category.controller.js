2fd8bb2a2463cfbeb5e50a4376f3f9e07b2e0dfa
Jagdish Patil <patil.jagdish222@gmail.com>
2024-04-14 18:27:40 +0530

    added getallbooksfromcategory api

--- node/projects/bookstore/src/controllers/category.controller.js
+++ node/projects/bookstore/src/controllers/category.controller.js
@@ -1,90 +1,112 @@
+import { Book } from "../models/book.model.js";
 import { Category } from "../models/category.model.js";
 import { ApiError } from "../utils/apiError.js";
 import { ApiResponse } from "../utils/apiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
-
-
 const createCategory = asyncHandler(async (req, res) => {
-    const {categoryName} = req.body
-    if(!categoryName) {
-        throw new ApiError(400, "Category name is required");
-    }
+  const { categoryName } = req.body;
+  if (!categoryName) {
+    throw new ApiError(400, "Category name is required");
+  }
 
-    const existedError = await Category.findOne({ $or: [{ categoryName }] });
-    if (existedError) {
-        throw new ApiError(400, "categoryName already exists")
-    }
+  const existedError = await Category.findOne({ $or: [{ categoryName }] });
+  if (existedError) {
+    throw new ApiError(400, "categoryName already exists");
+  }
 
-    const category = await Category.create({categoryName});
+  const category = await Category.create({ categoryName });
 
-    return res
+  return res
     .status(200)
     .json(new ApiResponse(200, category, "Category added successfully"));
-    
 });
 
 const getAllCategories = asyncHandler(async (req, res) => {
-    // Use the correct Mongoose method to find the book
-    const book = await Category.find({ });
-    if (!book) {
-        return res
-            .status(404)
-            .json(new ApiResponse(404, null, "Categories not found"));
-    }
-
+  // Use the correct Mongoose method to find the book
+  const book = await Category.find({});
+  if (!book) {
     return res
-        .status(200)
-        .json(new ApiResponse(200, book, "Categories fetched successfully"));
+      .status(404)
+      .json(new ApiResponse(404, null, "Categories not found"));
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, book, "Categories fetched successfully"));
 });
 
 const getCategoryById = asyncHandler(async (req, res) => {
-    const categoryId = req.query.categoryId;
-    // Use the correct Mongoose method to find the book
-    const book = await Category.findOne({ _id: categoryId });
-    if (!book) {
-        return res
-            .status(404)
-            .json(new ApiResponse(404, null, "categoryId not found"));
-    }
-
+  const categoryId = req.query.categoryId;
+  // Use the correct Mongoose method to find the book
+  const book = await Category.findOne({ _id: categoryId });
+  if (!book) {
     return res
-        .status(200)
-        .json(new ApiResponse(200, book, "categoryId fetched successfully"));
+      .status(404)
+      .json(new ApiResponse(404, null, "categoryId not found"));
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, book, "categoryId fetched successfully"));
 });
 
-
 const updateCategoryById = asyncHandler(async (req, res) => {
-    const categoryId = req.query.categoryId;
+  const categoryId = req.query.categoryId;
 
-    const category = await Category.findOne({ _id: categoryId });
+  const category = await Category.findOne({ _id: categoryId });
 
-    if (!category) {
-        return res.status(404).json(new ApiResponse(404, category, "Category not found"))
-    }
-
-    category.categoryName = req.body.categoryName || category.categoryName;
-
-    const updatedCategory = await category.save();
+  if (!category) {
     return res
-        .status(200)
-        .json(new ApiResponse(200, updatedCategory, "Category updated successfully"));
+      .status(404)
+      .json(new ApiResponse(404, category, "Category not found"));
+  }
 
+  category.categoryName = req.body.categoryName || category.categoryName;
+
+  const updatedCategory = await category.save();
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, updatedCategory, "Category updated successfully")
+    );
 });
 
 const deleteCategoryById = asyncHandler(async (req, res) => {
-    const categoryId = req.query.categoryId;
-    // Use the correct Mongoose method to find the book
-    const category = await Category.deleteOne({ _id: categoryId });
-    if (!category) {
-        return res
-            .status(404)
-            .json(new ApiResponse(404, null, "Category not found"));
-    }
-
+  const categoryId = req.query.categoryId;
+  // Use the correct Mongoose method to find the book
+  const category = await Category.deleteOne({ _id: categoryId });
+  if (!category) {
     return res
-        .status(200)
-        .json(new ApiResponse(200, category, "Category deleted successfully"));
+      .status(404)
+      .json(new ApiResponse(404, null, "Category not found"));
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, category, "Category deleted successfully"));
 });
 
-export {createCategory, getAllCategories, getCategoryById, updateCategoryById, deleteCategoryById}
+const getAllBooksFromCategory = asyncHandler(async (req, res) => {
+  const categoryId = req.query.categoryId;
+  // Use the correct Mongoose method to find the book
+  const book = await Book.findOne({ category: categoryId });
+  if (!book) {
+    return res
+      .status(404)
+      .json(new ApiResponse(404, null, "categoryId not found"));
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, book, "categoryId fetched successfully"));
+});
+
+export {
+  createCategory,
+  getAllCategories,
+  getCategoryById,
+  updateCategoryById,
+  deleteCategoryById,
+  getAllBooksFromCategory,
+};
--- node/projects/bookstore/src/routes/book.routes.js
+++ node/projects/bookstore/src/routes/book.routes.js
@@ -1,24 +1,36 @@
 import { Router } from "express";
-import { addBookHandler, deleteBookHandler, getAllBooksHandler, getBookHandler, searchBookHandler, updateBookHandler } from "../controllers/book.controller.js";
-import { createCategory, deleteCategoryById, getAllCategories, getCategoryById, updateCategoryById } from "../controllers/category.controller.js";
+import {
+  addBookHandler,
+  deleteBookHandler,
+  getAllBooksHandler,
+  getBookHandler,
+  searchBookHandler,
+  updateBookHandler,
+} from "../controllers/book.controller.js";
+import {
+  createCategory,
+  deleteCategoryById,
+  getAllBooksFromCategory,
+  getAllCategories,
+  getCategoryById,
+  updateCategoryById,
+} from "../controllers/category.controller.js";
 
 const router = Router();
 
-router.route('/addBook').post(addBookHandler)
-router.route('/getBook').get(getBookHandler);
-router.route('/deleteBookbyId').put(deleteBookHandler);
-router.route('/updateBookbyId').delete(updateBookHandler);
-router.route('/searchBook').get(searchBookHandler);
-router.route('/getBooks').get(getAllBooksHandler);
-
+router.route("/addBook").post(addBookHandler);
+router.route("/getBook").get(getBookHandler);
+router.route("/deleteBookbyId").put(deleteBookHandler);
+router.route("/updateBookbyId").delete(updateBookHandler);
+router.route("/searchBook").get(searchBookHandler);
+router.route("/getBooks").get(getAllBooksHandler);
+router.route("/getBooksFromCategoryId").get(getAllBooksFromCategory);
 
 // categories
-router.route('/createCategory').post(createCategory);
-router.route('/getCategories').get(getAllCategories);
-router.route('/getCategoriesbyId').get(getCategoryById);
-router.route('/updateCategorybyId').put(updateCategoryById);
-router.route('/deleteCategorybyId').delete(deleteCategoryById);
-
-
+router.route("/createCategory").post(createCategory);
+router.route("/getCategories").get(getAllCategories);
+router.route("/getCategoriesbyId").get(getCategoryById);
+router.route("/updateCategorybyId").put(updateCategoryById);
+router.route("/deleteCategorybyId").delete(deleteCategoryById);
 
 export default router;
