9aa84388bfd43595caae4dc2d80d79f76589c247
Jagdish Patil <patil.jagdish222@gmail.com>
2024-05-02 09:12:40 +0530

    fixed vercel config issue

--- node/projects/random-quote-generator/index.js
+++ node/projects/random-quote-generator/index.js
@@ -1,47 +1,21 @@
-// import connectDB from "./index";
+import { app } from "./src/app.js";
+import connectDB from "./src/db/index.js";
 import dotenv from "dotenv";
-import express from "express";
-import cors from "cors";
-const app = express();
 
 dotenv.config({
   path: "./env",
 });
 
-app.use(
-  cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true,
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`server is running at ${8000}`);
+    });
   })
-);
-
-// to limit the json respnse from backend
-app.use(
-  express.json({
-    limit: "16kb",
-  })
-);
-
-// make express aware of data coming in params
-app.use(
-  express.urlencoded({
-    extended: true,
-    limit: "16kb",
-  })
-);
-
-// routes import
-
-import randomQuoteRouter from "./src/router/quote.route.js";
-// routes declaration
-
-app.use("/api/v1/quote", randomQuoteRouter);
+  .catch((err) => {
+    console.log("mongodb connection failed", err);
+  });
 
 app.get("/", (req, res) => {
   res.send("random quote generator api");
-});
-
-// connectDB();
-app.listen(process.env.PORT || 8000, () => {
-  console.log(`server is running at ${8000}`);
 });
