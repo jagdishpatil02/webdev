f32f31d864b0d781a5c230dcee86ec61530ef495
Jagdish Patil <patil.jagdish222@gmail.com>
2024-05-20 09:40:29 +0530

    added vercel json config

--- node/projects/bookstore/src/app.js
+++ node/projects/bookstore/src/app.js
@@ -1,12 +1,14 @@
 import express from "express";
 import cookieParser from "cookie-parser";
-import cors from "cors"
-const app = express()
+import cors from "cors";
+const app = express();
 
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.CORS_ORIGIN,
-    credentials: true
-}));
+    credentials: true,
+  })
+);
 
 // to limit the json respnse from backend
 app.use(express.json());
@@ -14,16 +16,19 @@ app.use(express.json());
 // make express aware of data coming in params
 app.use(express.urlencoded({ extended: true }));
 
-
 // to store temp assets like favicon, images in public folder
 
-app.use(express.static("public"))
+app.use(express.static("public"));
 
 // to read and write cookie securly only from server
-app.use(cookieParser())
+app.use(cookieParser());
 
-import bookRoutes from './routes/book.routes.js'
+import bookRoutes from "./routes/book.routes.js";
 
-app.use('/api/v1/book', bookRoutes)
+app.use("/api/v1/book", bookRoutes);
 
-export {app}
+app.get("/", (req, res) => {
+  res.send("book store apis");
+});
+
+export { app };
